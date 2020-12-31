182c58f63022f9de46a2a71ec4d0dec53232ad76
wy1024 <3172508952@qq.com>
2020-12-31 20:44:58 +0800

    类和对象

---
+++ 类和对象/src/TestDemo.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Administrator
+ * Date: 2020-11-29
+ * Time: 14:30
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+
+    }
+}
